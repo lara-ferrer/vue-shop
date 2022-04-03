7facfb9a8cf2318e8ea5057d2adee4ad9125228e
Lara <larafpics@gmail.com>
2022-04-03 20:28:37 +0200

    chore: add cart component

---
+++ src/components/Cart.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="cart">
+        <p>Carrito</p>
+        <router-link to="/checkout" class="button">Checkout</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Cart',
+  
+}
+</script>
+
+<style scoped>
+    .cart {
+        background-color: #fff;
+        border: 1px solid #000;
+        position: absolute;
+        top: 5rem;
+        right: 1rem;
+        width: 200px;
+        padding: 2em;
+        color: #000;
+    }
+</style>
