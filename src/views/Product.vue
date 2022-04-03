e75ef372d5c46877b071a18a2388bcf59be8dbff
Lara <larafpics@gmail.com>
2022-04-03 20:26:39 +0200

    chore: add single product view

---
+++ src/views/Product.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="product">
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="12" lg="6">
+          <h1>{{ $route.params.name }}</h1>
+          <img :src="`${$route.params.image}`" class="product__image" :alt="`${$route.params.name}`">
+        </b-col>
+        <b-col sm="12" lg="6">
+          <p>Descripción</p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+  props: {
+    name: String,
+    image: String,
+  },
+}
+</script>
+
+<style scoped>
+  .product__image {
+    width: 100%;
+  }
+</style>
