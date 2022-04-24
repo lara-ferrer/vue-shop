16003cd3373ff930b2d340489b396bb12c4ad832
Lara <larafpics@gmail.com>
2022-04-24 15:30:04 +0200

    chore: add checkout in routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Product from '../views/Product.vue'
+import Checkout from '../views/Checkout.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/:slug',
     name: 'Product',
     component: Product
+  },
+  {
+    path: '/checkout',
+    name: 'Checkout',
+    component: Checkout
   },
 ]
 
