630a14878dc4d2fc10a2f5e8ed53b6b22da63127
Lara <larafpics@gmail.com>
2022-04-24 15:29:08 +0200

    chore: add CartItem

---
+++ src/components/Cart/CartItem.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="cart__item">
+      <img :src="`${getProductInfo.image}`" :alt="`${getProductInfo.name}`" class="cart__item__image">
+      {{ getProductInfo.name }} x {{ this.quantity }}
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'CartItem',
+  props: {
+    productId: Number,
+    quantity: Number
+  },
+  computed: {
+    ...mapGetters(['products']),
+    getProductInfo() {
+      return this.products.filter(product => product.id === this.productId)[0]
+    }
+  },
+}
+</script>
+
+<style scoped>
+  .cart__item__image {
+    width: 10%;
+  }
+</style>
