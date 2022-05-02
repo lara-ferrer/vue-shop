ca2c4e1983a0c589c9985efaf8ad1438d51de613
Lara <larafpics@gmail.com>
2022-05-03 00:28:03 +0200

    chore: display message in checkout if there are no items in the cart

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -2,17 +2,22 @@
   <b-container>
     <b-row>
       <b-col sm="12" md="8">
-        <h1>Checkout</h1>
-        <div v-for="product in cart" :key="product.id">
-          <CartItem
-            :id="product.id"
-            :productId="product.productId"
-            :quantity="product.quantity"
-          />
+        <h1 class="checkout__title">Checkout</h1>
+        <div v-if="cart.length">
+          <div v-for="product in cart" :key="product.id">
+            <CartItem
+              :id="product.id"
+              :productId="product.productId"
+              :quantity="product.quantity"
+            />
+          </div>
+        </div>
+        <div v-else>
+          <p class="checkout__no-products">No hay productos en el carrito</p>
         </div>
       </b-col>
-      <b-col sm="12" md="4">
-        <p>TOTAL</p>
+      <b-col sm="12" md="4" class="checkout__total">
+        <h2>Total</h2>
         <CartTotal />
       </b-col>
     </b-row>
@@ -33,19 +38,24 @@ export default {
   computed: {
     ...mapGetters(['cart']),
   },
-  head: {
-    title: {
-      inner: 'Checkout',
-      separator: '|',
-      complement: 'Caseland'
-    },
-    meta: [
-      { name: 'description', content: 'Si has elegido ya tus productos, puedes hacer el pago ahora.' }
-    ]
+  mounted() {
+    document.title = 'Checkout | Caseland'
   },
 }
 </script>
 
 <style scoped>
-  
+  .checkout__title {
+    text-align: left;
+    margin-bottom: 2rem;
+  }
+
+  .checkout__no-products {
+    text-align: left;
+  }
+
+  .checkout__total {
+    text-align: left;
+    border-left: 1px solid rgb(166, 166, 166);
+  }
 </style>
