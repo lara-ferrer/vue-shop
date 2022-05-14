ca39a0d7359722875ca65db66e243329efa42a09
Lara <larafpics@gmail.com>
2022-05-14 19:22:26 +0200

    chore: add categories in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,29 +1,67 @@
 <template>
-    <footer class="footer">
-        <img src="../assets/images/logo.png" class="footer__logo" alt="Logotipo Caseland">
-        <p class="footer__copy">&copy; 2022 Lara Ferrer</p>
-    </footer>
+  <footer class="footer">
+    <b-container>
+      <b-row>
+        <b-col sm="12" md="4">
+          <img
+            src="../assets/images/logo.png"
+            class="footer__logo"
+            alt="Logotipo Caseland"
+          />
+        </b-col>
+        <b-col sm="12" md="4" class="footer__section">
+          <h3>Comprar por categoría</h3>
+          <div v-for="category in categories" :key="category.id">
+            <router-link
+              :to="{
+                name: 'Category',
+                params: category.id,
+              }"
+              class="footer__link"
+            >
+              <span>Carcasas y fundas para {{ category.name }}</span>
+            </router-link>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </footer>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
-  name: 'Footer',
-}
+  name: "Footer",
+  computed: {
+    ...mapGetters(["categories"]),
+  },
+};
 </script>
 
 <style scoped>
-  .footer {
-    background-color: #864EBE;
-    color: #fff;
-    padding: 2rem 0;
-    text-align: center;
-  }
-  
-  .footer__logo {
-    width: 10%;
-  }
+.footer {
+  background-color: #864ebe;
+  color: #fff;
+  padding: 3rem 0;
+  text-align: center;
+}
 
-  .footer__copy {
-    margin-top: 1rem;
-  }
+.footer__logo {
+  width: 50%;
+}
+
+.footer__section {
+  text-align: left;
+}
+
+.footer__link {
+  color: #fff;
+  transition: 0.4s ease;
+}
+
+.footer__link:hover {
+  color: rgba(255, 255, 255, 0.71);
+  transition: 0.4s ease;
+}
 </style>
